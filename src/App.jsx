599aaebda0a8a3e201323adf74d4bb61f88747a3
Sayyed Shabbar Raza <syedshabbarraza207@gmail.com>
2025-04-19 21:00:21 +0500

    file structuring

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import SignIn from './pages/signIn.jsx';
+import Home from './pages/home.jsx';
+import SignUp from './pages/signUp.jsx';
+import Profile from './pages/profile.jsx';
+import About from './pages/about.jsx';
+function App() {
+
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="sign-in" element={<SignIn/>}/>
+    <Route path="sign-Up" element={<SignUp />}/>
+    <Route path="profile" element={<Profile />}/>
+    <Route path="/about" element={<About />}/>
+  </Routes>
+  </BrowserRouter>
+}
+
+export default App
