eb3718ff933571e536746a87ba3d70f796da367a
Sayyed Shabbar Raza <syedshabbarraza207@gmail.com>
2025-05-28 22:19:29 +0500

    lasttt

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const res = await fetch(`${API_BASE}/listing/get?type=sale&limit=4`);
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
