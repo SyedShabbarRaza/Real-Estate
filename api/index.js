5cfd28b873cf076f8913163c3bc00d7484a7435b
Sayyed Shabbar Raza <syedshabbarraza207@gmail.com>
2025-05-29 18:16:32 +0500

    cors

--- api/index.js
+++ api/index.js
@@ -4,9 +4,10 @@ import authRouter from './routes/auth_route.js';
 import userRouter from './routes/user_route.js';
 import listingRouter from './routes/listing_route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 const app=express();
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
